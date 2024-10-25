93738876461891d8259fd6a39a5d0fcedfa4e3b9
seyyeDevops <seyedmahditayeb@gmail.com>
2024-10-25 12:59:38 +0330

    update docker compose file

--- roles/rsyslog-service/files/log_sender.sh
+++ roles/rsyslog-service/files/log_sender.sh
@@ -1,16 +1,16 @@
-            #!/bin/bash
+#!/bin/bash
 
-            FACILITY="local0"
-            PRIORITY="info"
-            while true; do
-                HEADER="myfirsylog: $(date +%Y-%m-%dT%H:%M:%S) hello devops"
-                POPULATION=$(( 10000 + $RANDOM % 1000000))
-                MEN=$((POPULATION / 2))
-                RANDOMSHORTINT=$((RANDOM % 50 + 1))
-                WOMEN=$((POPULATION/RANDOMSHORTINT))
-                MESSAGE="{\"iran\": {\"city\": \"tehran\", \"POPULATION\": \"${POPULATION}\", \"men\": \"${MEN}\", \"women\": \"${WOMEN}\", \"hOffset\": \"2000\", \"vOffset\": \"100\", \"weather\": \"sun1.city = (sun1.city / 100) * 90;\"}}"
+FACILITY="local0"
+PRIORITY="info"
+while true; do
+    HEADER="myfirsylog: $(date +%Y-%m-%dT%H:%M:%S) hello devops"
+    POPULATION=$(( 10000 + $RANDOM % 1000000))
+    MEN=$((POPULATION / 2))
+    RANDOMSHORTINT=$((RANDOM % 50 + 1))
+    WOMEN=$((POPULATION/RANDOMSHORTINT))
+    MESSAGE="{\"iran\": {\"city\": \"tehran\", \"POPULATION\": \"${POPULATION}\", \"men\": \"${MEN}\", \"women\": \"${WOMEN}\", \"hOffset\": \"2000\", \"vOffset\": \"100\", \"weather\": \"sun1.city = (sun1.city / 100) * 90;\"}}"
 
-                sleep 2
-                logger -n 127.0.0.1 -P 5140 -T -t "${HEADER}" -p "${FACILITY}.${PRIORITY}" "${MESSAGE}"
+    sleep 2
+    logger -n 127.0.0.1 -P 5140 -T -t "${HEADER}" -p "${FACILITY}.${PRIORITY}" "${MESSAGE}"
 
-            done
+done
